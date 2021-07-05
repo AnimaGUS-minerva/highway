d3a46c477727ab177799455e61860f740e178857
Michael Richardson <mcr@sandelman.ca>
2021-07-05 13:54:44 -0400

    added hostname to version return

--- app/controllers/version_controller.rb
+++ app/controllers/version_controller.rb
@@ -5,12 +5,13 @@ class VersionController < ActionController::Base
     respond_to do |format|
       format.html {
         # yes, they asked for text/html, but we are giving them text/plain
-        render :plain => "highway:\n\tVersion: #{$VERSION}\n\tRevision: #{$REVISION}\n", :content_type => 'text/plain'
+        render :plain => "highway:\n\tHostname: #{SystemVariable.hostname}\n\tVersion: #{$VERSION}\n\tRevision: #{$REVISION}\n", :content_type => 'text/plain'
       }
       format.json {
         data = Hash.new
         data['version'] = $VERSION
         data['revision']= $REVISION
+        data['Hostname'] = SystemVariable.hostname,
         api_response(data, :ok, 'application/json')
       }
     end

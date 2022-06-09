8361c866aa2e2bce303a8b0cae0c2e0d352767e7
Michael Richardson <mcr@sandelman.ca>
2022-06-09 14:58:43 -0400

    docs: remove dead deployment comments

--- config/deploy/wheezes.rb
+++ config/deploy/wheezes.rb
@@ -32,16 +32,3 @@ set :ssh_options, {
       forward_agent: true,
     }
 
-#
-# The server-based syntax can be used to override options:
-# ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }

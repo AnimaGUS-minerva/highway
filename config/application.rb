e443e58154ba3934e7a3bc854a42ef689468d533
Michael Richardson <mcr@sandelman.ca>
2025-04-14 14:15:32 -0400

    feat: upgrade to rails 8.0.2

--- config/application.rb
+++ config/application.rb
@@ -27,9 +27,7 @@ module Highway
 
     config.autoload_lib(ignore: %w(assets tasks))
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.active_support.to_time_preserves_timezone = :zone
 
   end
 end
